975f9ae7bdf4d9a5fcd766834fce556c2985bb83
CelionLigalamu <ligalamucelion@gmail.com>
2026-09-04 01:17:58 +0300

    phase-6: auto-generate reference numbers on member creation via admin

--- members/admin.py
+++ members/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Member
+from .services import generate_reference_number
 
-admin.site.register(Member)
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('reference_number', 'full_name', 'phone_number', 'church', 'status')
+    readonly_fields = ('reference_number',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.reference_number:
+            obj.reference_number = generate_reference_number(obj.church_id)
+        super().save_model(request, obj, form, change)
